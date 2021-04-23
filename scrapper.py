5164dc0903ea459a6629a462c38cb5a2cb2eebe0
Sayaji911 <sayajichavan9999@gmail.com>
2021-04-23 16:40:25 +0530

    Add files via upload

--- scrapper.py
+++ scrapper.py
@@ -2,22 +2,24 @@ import requests as _requests
 import bs4 as _bs4
 from typing import List, Dict
 import json as _json
-import  pandas as pd
+import itertools
+
 import csv
 
 
-
-
-def _generate_url(track_id : str) -> str:
+def _generate_url(track_id: str) -> str:
     url = f"https://ekartlogistics.com/track/{track_id}"
     return url
 
-def _get_page(url : str) -> _bs4.BeautifulSoup:
+
+def _get_page(url: str) -> _bs4.BeautifulSoup:
     page = _requests.get(url)
-    soup = _bs4.BeautifulSoup(page.content,  "html.parser")
+    soup = _bs4.BeautifulSoup(page.content, "html.parser")
     return soup
 
+
 def complete_tracking(track_id: str):
+    raw_list = []
     headers = []
     _data = []
     url = _generate_url(track_id)
@@ -28,47 +30,54 @@ def complete_tracking(track_id: str):
         title = i.text.strip()
         headers.append(title)
 
-
-
     for row in table.find_all('tr')[1:]:
         data = row.find_all('td')
         row_data = [td.text.strip() for td in data]
-        _data.append(row_data)
+        raw_list.append(row_data)
+#    return raw_list
 
-        for n in range(20):
-            data_zip = dict(zip(headers, row_data))
-
-
-        with open("events.json", mode = "w") as events_file:
-              _json.dump(data_zip, events_file, indent=False)
-
+    for i in range(len(raw_list)):
+        zipped_ = dict(zip(headers, raw_list[i]))
+        _data.append(zipped_)
+    return _data
 
 
-print(complete_tracking('FMPP0701772870'))
+'''      #  for n in range(20):
+       #     data_zip = dict(zip(headers, row_data)) mm,
+
+       for i in range(0, len(headers)):
+            jsonList.append({"header": headers[i], "status": row_data[i]})
+
+    return jsonList
+'''
+
+print(complete_tracking('FMPP0701772820'))
+
 
 def current_status(track_id: str):
     headers = []
     url = _generate_url(track_id)
     page = _get_page(url)
-    #gets table from the url
+    # gets table from the url
     table = page.find('table', {'class': 'col-md-12 table-bordered table-striped table-condensed cf width-100'})
-    #puts the table headers in header list
+    # puts the table headersuvi in header list
     for i in table.find_all('th'):
         title = i.text.strip()
         headers.append(title)
-    #puts all data in row data
+    # puts all data in row data
     for row in table.find_all('tr')[1:]:
-        data  = row.find_all('td')
+        data = row.find_all('td')
         row_data = [td.text.strip() for td in data]
-    #covert list data to json
-    jsonList = []
+        # covert list data to json
+        _data_one = []
+        for i in range(len(headers)):
+            zipped_one = dict(zip(headers, row_data))
+        _data_one.append(zipped_one)
+        return _data_one
+
+
+'''    jsonList = []
     for i in range(0, len(headers)):
         jsonList.append({"header": headers[i], "status": row_data[i]})
     return jsonList
-
-
-
-
-
-
-
+'''
